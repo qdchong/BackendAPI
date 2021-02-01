09c482d95a09aed1cce26251817c4c9f6d1739bf
qdchong <qidachong@gmail.com>
2021-02-01 14:47:41 +0800

    Create Boilerplate

---
+++ api/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+
+from api.config import env_config
+
+api = Api()
+
+
+def create_app(config_name):
+    import resources
+
+    app = Flask(__name__)
+
+    app.config.from_object(env_config[config_name])
+    api.init_app(app)
+
+    return app
---
+++ api/config.py
@@ -0,0 +1,38 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DEV_DATABASE_URL"
+    )
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "TEST_DATABASE_URL"
+    )
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    )
+
+
+env_config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig
+}
